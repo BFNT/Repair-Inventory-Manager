64f4ad108e9f08d9a442ecfd50eff55b3a3f1052
JackCarterSmith <arechristopher1@gmail.com>
2017-07-08 12:27:43 +0200

    Added new GUI elements to IDGenerator form

--- ISEN-Repair Inventory Manager/IDGenerator.Designer.vb
+++ ISEN-Repair Inventory Manager/IDGenerator.Designer.vb
@@ -27,7 +27,16 @@ Partial Class IDGenerator
         Me.GenerateNewIDButton = New System.Windows.Forms.Button()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.newRandomIDBox = New System.Windows.Forms.MaskedTextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.IDListBox = New System.Windows.Forms.ListBox()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewControl1 = New System.Windows.Forms.PrintPreviewControl()
+        Me.PrintButton = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Barcode1
@@ -56,7 +65,7 @@ Partial Class IDGenerator
         Me.Barcode1.DoPaint = True
         Me.Barcode1.FitControlToBarcode = True
         Me.Barcode1.LeftMarginCM = 0.2!
-        Me.Barcode1.Location = New System.Drawing.Point(25, 53)
+        Me.Barcode1.Location = New System.Drawing.Point(21, 57)
         Me.Barcode1.Name = "Barcode1"
         Me.Barcode1.NarrowToWideRatio = 2.0!
         Me.Barcode1.OneBitPerPixelImage = False
@@ -84,7 +93,7 @@ Partial Class IDGenerator
         '
         'GenerateNewIDButton
         '
-        Me.GenerateNewIDButton.Location = New System.Drawing.Point(12, 171)
+        Me.GenerateNewIDButton.Location = New System.Drawing.Point(8, 175)
         Me.GenerateNewIDButton.Name = "GenerateNewIDButton"
         Me.GenerateNewIDButton.Size = New System.Drawing.Size(198, 39)
         Me.GenerateNewIDButton.TabIndex = 0
@@ -95,7 +104,7 @@ Partial Class IDGenerator
         '
         Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(8, 16)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(198, 127)
         Me.PictureBox1.TabIndex = 3
@@ -105,31 +114,100 @@ Partial Class IDGenerator
         '
         Me.newRandomIDBox.AllowDrop = True
         Me.newRandomIDBox.AsciiOnly = True
-        Me.newRandomIDBox.Location = New System.Drawing.Point(64, 145)
+        Me.newRandomIDBox.Location = New System.Drawing.Point(60, 149)
         Me.newRandomIDBox.Mask = "9999999999"
         Me.newRandomIDBox.Name = "newRandomIDBox"
         Me.newRandomIDBox.Size = New System.Drawing.Size(100, 20)
         Me.newRandomIDBox.TabIndex = 1
         Me.newRandomIDBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Barcode1)
+        Me.GroupBox1.Controls.Add(Me.newRandomIDBox)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Controls.Add(Me.GenerateNewIDButton)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(216, 226)
+        Me.GroupBox1.TabIndex = 101
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Simple"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.PrintButton)
+        Me.GroupBox2.Controls.Add(Me.PrintPreviewControl1)
+        Me.GroupBox2.Controls.Add(Me.IDListBox)
+        Me.GroupBox2.Location = New System.Drawing.Point(234, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(471, 226)
+        Me.GroupBox2.TabIndex = 102
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Multiple"
+        '
+        'IDListBox
+        '
+        Me.IDListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.IDListBox.FormattingEnabled = True
+        Me.IDListBox.Location = New System.Drawing.Point(6, 16)
+        Me.IDListBox.Name = "IDListBox"
+        Me.IDListBox.ScrollAlwaysVisible = True
+        Me.IDListBox.SelectionMode = System.Windows.Forms.SelectionMode.None
+        Me.IDListBox.Size = New System.Drawing.Size(112, 197)
+        Me.IDListBox.TabIndex = 0
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.AllowCurrentPage = True
+        Me.PrintDialog1.AllowPrintToFile = False
+        Me.PrintDialog1.AllowSomePages = True
+        Me.PrintDialog1.Document = Me.PrintDocument1
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'PrintDocument1
+        '
+        Me.PrintDocument1.DocumentName = "NewIDTemplate"
+        '
+        'PrintPreviewControl1
+        '
+        Me.PrintPreviewControl1.AutoZoom = False
+        Me.PrintPreviewControl1.Document = Me.PrintDocument1
+        Me.PrintPreviewControl1.Location = New System.Drawing.Point(124, 16)
+        Me.PrintPreviewControl1.Name = "PrintPreviewControl1"
+        Me.PrintPreviewControl1.Size = New System.Drawing.Size(341, 153)
+        Me.PrintPreviewControl1.TabIndex = 1
+        Me.PrintPreviewControl1.Zoom = 0.35R
+        '
+        'PrintButton
+        '
+        Me.PrintButton.Location = New System.Drawing.Point(213, 182)
+        Me.PrintButton.Name = "PrintButton"
+        Me.PrintButton.Size = New System.Drawing.Size(165, 31)
+        Me.PrintButton.TabIndex = 2
+        Me.PrintButton.Text = "Imprimer"
+        Me.PrintButton.UseVisualStyleBackColor = True
+        '
         'IDGenerator
         '
         Me.AcceptButton = Me.GenerateNewIDButton
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(224, 217)
-        Me.Controls.Add(Me.newRandomIDBox)
-        Me.Controls.Add(Me.GenerateNewIDButton)
-        Me.Controls.Add(Me.Barcode1)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.ClientSize = New System.Drawing.Size(714, 243)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(730, 282)
         Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(730, 282)
         Me.Name = "IDGenerator"
         Me.ShowIcon = False
         Me.Text = "Générer de nouveaux IDs"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -137,4 +215,11 @@ Partial Class IDGenerator
     Friend WithEvents GenerateNewIDButton As Button
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents newRandomIDBox As MaskedTextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents IDListBox As ListBox
+    Friend WithEvents PrintPreviewControl1 As PrintPreviewControl
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintButton As Button
 End Class
--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 ## Modification to do for the project
 - Add validation part for can't modify computer progress when it's to be send out
 - Add logger for all operations
-- Update ID Generator for more generation at a time
+- Update ID Generator for more generation at a time and printing function
 - Modify add computer function to add a full serial of computer (gain of time)
 - Add option to access at DB location (for backup ex.)
