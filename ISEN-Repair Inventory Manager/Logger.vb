b170a860bed06fefa9970aea1dfdede58261d52e
JackCarterSmith <arechristopher1@gmail.com>
2017-07-08 17:24:17 +0200

    Begin implementation of logger in the software

--- ISEN-Repair Inventory Manager/Logger.vb
+++ ISEN-Repair Inventory Manager/Logger.vb
@@ -5,12 +5,12 @@ Public Class Logger
 
     Dim logFileName As String = "logs\" & DateTime.Now.ToString("yyyy-MM-dd") & ".txt"
 
-    Shared Sub Load()
+    Public Sub Load()
         If Not My.Computer.FileSystem.DirectoryExists("logs") Then My.Computer.FileSystem.CreateDirectory("logs")
         If Not My.Computer.FileSystem.FileExists(logFileName) Then File.WriteAllLines(logFileName, {"LOG FILE - " & DateTime.Now.ToString("dd/MM/yyyy"), "--------------------", "[INFO] Loading software"})
     End Sub
 
-    Sub Info(str As String)
+    Public Sub Info(str As String)
         Dim objWriter As New StreamWriter(logFileName, True)
         Try
             objWriter.WriteLineAsync("[INFO] " & str)
@@ -20,7 +20,7 @@ Public Class Logger
         objWriter.Close()
     End Sub
 
-    Sub Warn(str As String)
+    Public Sub Warn(str As String)
         Dim objWriter As New StreamWriter(logFileName, True)
         Try
             objWriter.WriteLineAsync("[WARN] " & str)
@@ -30,7 +30,7 @@ Public Class Logger
         objWriter.Close()
     End Sub
 
-    Sub Critical(str As String)
+    Public Sub Critical(str As String)
         Dim objWriter As New StreamWriter(logFileName, True)
         Try
             objWriter.WriteLineAsync("[CRITICAL] " & str)
